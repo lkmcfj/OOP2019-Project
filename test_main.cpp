86a1e366e92861a0897e6afa040a26688205006e
lkmcfj <lkmcfj@outlook.com>
2019-04-30 17:33:06 +0800

    add test code

---
+++ test_main.cpp
@@ -0,0 +1,34 @@
+#include "computational_graph.h"
+#include <iostream>
+#include <string>
+using namespace computational_graph;
+int main()
+{
+    Graph default_graph;
+    Parser default_parser;
+    int n;
+    std::cin>>n;
+    while(n--)
+    {
+        std::string input_s;
+        std::cin.getline(input_s);
+        default_parser.start(input_s,&default_graph);
+    }
+    int m;
+    std::cin>>m;
+    while(m--)
+    {
+        std::string input_s;
+        std::cin.getline(input_s);
+        default_parser.node(input_s,&default_graph);
+    }
+    Session default_sess(default_graph);
+    int q;
+    std::cin>>q;
+    while(q--)
+    {
+        std::string input_s;
+        std::cin.getline(input_s);
+        default_parser.run(input_s,&default_sess);
+    }
+}
