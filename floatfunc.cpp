c2dc0eb5ffa1c1824677eea91516798c9c1d0201
lkmcfj <lkmcfj@outlook.com>
2019-06-11 22:52:29 +0800

    split function for float number into another file

---
+++ floatfunc.cpp
@@ -0,0 +1,86 @@
+#include "floatfunc.h"
+#include <string>
+#include <cstdio>
+
+namespace computational_graph
+{
+	TODO:implement of FloatSetting
+	TODO:implement of double_string
+	bool double_boolean(double v)
+	{
+		return v>FloatSetting::get_eps();
+	}
+	
+	double double_plus(double x,double y)
+    {
+        return x+y;
+    }
+    double double_minus(double x,double y)
+    {
+        return x-y;
+    }
+    double double_multi(double x,double y)
+    {
+        return x*y;
+    }
+    double double_div(double x,double y)
+    {
+		if(std::abs(y)<FloatSetting::get_eps())
+		{
+			Message::message("ERROR: Division by zero");
+			throw std::range_error("Division by zero");	
+		}
+		return x/y;
+	}
+	
+	double double_sin(double x)
+    {
+		return std::sin(x);
+	}
+	double double_diff_sin(double x)
+    {
+		return std::cos(x);
+	}
+	double double_log(double x)
+	{
+		if(x<=0)
+		{
+			Message::message("ERROR: LOG operator's input must be positive");
+			throw std::range_error("LOG operator's input must be positive");
+		}
+		return std::log(x);
+	}
+	double double_diff_log(double x)
+	{
+		if(x<=0)
+		{
+			Message::message("ERROR: LOG operator's input must be positive");
+			throw std::range_error("LOG operator's input must be positive");
+		}
+		return 1/x;
+	}
+	double double_exp(double x)
+	{
+		return std::exp(x);
+	}
+	double double_diff_exp(double x)
+	{
+		return std::exp(x);
+	}
+	double double_tanh(double x)
+	{
+		return std::tanh(x);
+	}
+	double double_diff_tanh(double x)
+	{
+		double t=std::exp(x)+std::exp(-x);
+		return 4/(t*t);
+	}
+	double double_sigmoid(double x)
+	{
+		return 1/(1+std::exp(-x));
+	}
+	double double_diff_sigmoid(double x)
+	{
+		return 1/(std::exp(x)+2+std::exp(-x));
+	}
